d0af1c43e1fb75ab2ebe49b50326490559035c46
Nikita Kuzmenko <kuzmenko.ns@mail.ru>
2021-10-26 19:22:32 +0000

    Modified makefile
    Closes #1

--- 3DVecSrc/makefile.mk
+++ 3DVecSrc/makefile.mk
@@ -6,11 +6,11 @@ all: program
 program: main.o Vector3D.o
 	gcc main.o Vector3D.o -o program
 
-main.o: main.c 
-	gcc -c main.c
+%.o: %.c
+	gcc -c $^ -o $@
+#main.o: main.c #gcc -c main.c
 
-Vector3D.o: Vector3D.c 
-	gcc -c Vector3D.c
+#Vector3D.o: Vector3D.c  #gcc -c Vector3D.c
 
 
 
